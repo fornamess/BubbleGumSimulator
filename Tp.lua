1d6b9518cd714ee5a67f1164c01a66941548252a
fornamess <Romanpostik123@gmail.com>
2025-04-20 16:54:18 +0500

    Update Tp.lua

--- Tp.lua
+++ Tp.lua
@@ -1,1 +1,1 @@
-game:GetService('TeleportService'):TeleportToPlaceInstance(920587237, '13bbd794-c7e3-4470-8a2a-72f98b77fae0', game.Players.LocalPlayer) 
+game:GetService('TeleportService'):TeleportToPlaceInstance(920587237, '8ba3bc68-9d33-44e7-a8ec-9b5a207a16d9', game.Players.LocalPlayer)
